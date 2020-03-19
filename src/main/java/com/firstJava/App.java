95c6331adaf418140e09ca07e875cd02691c8f30
Rafi <raficse@gmail.com>
2020-03-19 13:57:02 +0530

    App.java & AppTest.java added

---
+++ src/main/java/com/firstJava/App.java
@@ -0,0 +1,17 @@
+package com.firstJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+	public int calculateSomething() {
+		return 0;
+	}
+}
---
+++ src/test/java/com/firstJava/AppTest.java
@@ -0,0 +1,14 @@
+package com.fristJava;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest 
+{
+	@Test
+	public void testApp()
+    {
+        assertEquals(0,new App().calculateSomething());
+    }
+}
